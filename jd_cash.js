d7ba3e432e5a6e6c7cfaed0e16cd160cfc19fa53
alumorr <alumorr@gmail.com>
2021-10-09 00:30:43 +0000

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -31,7 +31,7 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = true;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `cU5-Oa7kN6JJrA@cU5-Oa7kN6Jl`
+  `cU5-Oa7kN6JJrA@cU5-Oa7kN6Jl`,
   `cU5-Oa7kN6JJrA@cU5-Oa7kN6Jl`
 ]
 $.authorCode = []
