d9e26732cdf925b8f30a7f1ec588ded972736ba6
alumorr <alumorr@gmail.com>
2021-10-31 23:49:08 +0800

    Add files via upload

--- jd_qjd.js
+++ jd_qjd.js
@@ -5,6 +5,7 @@ cron 0 0,12,20 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/j
  * */
 const $ = new Env('qjd');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
@@ -27,9 +28,9 @@ let autoCodeList = []
         return;
     }
     let res = [];
-    try{res = await getAuthorShareCode('https://');}catch (e) {}
+    try{res = await getAuthorShareCode(' ');}catch (e) {}
     if(!res){
-        try{res = await getAuthorShareCode('https://');}catch (e) {}
+        try{res = await getAuthorShareCode(' ');}catch (e) {}
         if(!res){res = [];}
     }
     if(res.length > 0){
